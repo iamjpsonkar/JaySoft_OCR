3546dd3fb574e43830600c19c57f9bab0a59204d
Jay Prakash Sonkar <iamjpsonkar@gmail.com>
2024-02-10 18:26:27 +0530

    added external call

--- v2.0/backend/ocr/ocrapp.py
+++ v2.0/backend/ocr/ocrapp.py
@@ -2,9 +2,9 @@ from sanic.views import HTTPMethodView
 from sanic.response import json
 from sanic.exceptions import FileNotFound
 import base64
-# from app import logger
+import httpx
 import logging
-
+from .ocr_secrets import ocr_api, apikey
 # logger = getLogger()
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
@@ -35,16 +35,27 @@ class OCR(HTTPMethodView):
             if not base64_image:
                 raise FileNotFound("Image data is missing")
 
-            # Decode base64 image data
-            image_data = base64.b64decode(base64_image)
+            # # Decode base64 image data
+            # image_data = base64.b64decode(base64_image)
 
-            # Save the decoded image to a desired location
-            filename = 'uploaded_image.png'  # Example filename
-            with open("uploads/"+filename, 'wb') as f:
-                f.write(image_data)
+            # # Save the decoded image to a desired location
+            # filename = 'uploaded_image.png'  # Example filename
+            # with open("uploads/"+filename, 'wb') as f:
+            #     f.write(image_data)
 
-            # Respond with a success message or any other data
-            return json({'message': 'Image uploaded successfully'})
+            # # Respond with a success message or any other data
+            # return json({'message': 'Image uploaded successfully'})
+            data_to_send = {
+                "apikey": apikey,
+                "base64Image" : base64_image,
+                "OCREngine": 3
+            }
+            
+            async with httpx.AsyncClient() as client:
+                response = await client.post(ocr_api, json=data_to_send)
+                response_data = response.json()
+            logger.info("ocr_api_response",response_data)
+            return json(response_data)
         except FileNotFound as e:
             logger.info({'error': str(e)}, 400)
             return json({'error': str(e)}, status=400)
--- v2.0/ocr/src/components/OcrResult/OcrResult.js
+++ v2.0/ocr/src/components/OcrResult/OcrResult.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
+import ReactJson from 'react-json-view';
 
 const OcrResult = () => {
     // Access the OCR result data from location state
     const location = useLocation();
     const { resultData } = location.state || {};
 
-    return (
-        <div className="text-center">
-            <h2 className="mb-4">OCR Result</h2>
+    return ( 
+    
+        <div>
+            <h2 className="mb-4 text-center">OCR Result</h2>
             {/* Display the OCR result data */}
-            <p>OCR Result: {resultData && resultData.message}</p>
+            {resultData && (
+                <div>
+                    <ReactJson src={resultData} collapsed={1} />
+                </div>
+            )}
         </div>
     );
 };
